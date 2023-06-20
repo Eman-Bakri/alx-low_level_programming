f8df16f71f47552c24216ab2b686351307daa2f0
Eman-Bakri <eman1996-sy@hotmail.com>
2023-06-19 22:16:51 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Shows the absolute value
+ *
+ * @i: The integer input
+ *
+ *Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
