f840184345f0e7b7ebe69133db8df1c2217cc53a
Eman-Bakri <eman1996-sy@hotmail.com>
2023-08-07 16:58:42 -0700

    display information in ELF header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,12 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-void check_elf(unsigned char *e_ident);
+/*
+ * e_ident[] - represets IDENTIFICATION INDEXES
+ *
+ * Description: The initial bytes makes the file be an object file
+ *              and give machine-independent data.
+*/
+void check_for_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
@@ -16,14 +16,13 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 /**
- * check_elf - Checks whethre a file is an ELF file or not.
+ * check_for_elf - Checks whether a file is an ELF file or not.
  * @e_ident: A pointer to an array having ELF magic nums.
  *
  * Description: If not an ELF file - exit code 98.
  */
-void check_elf(unsigned char *e_ident)
+void check_for_elf(unsigned char *e_ident)
 {
 	int in;
 
