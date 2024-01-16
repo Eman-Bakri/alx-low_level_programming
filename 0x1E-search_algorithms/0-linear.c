3ae41688f35e4d7e2f746a97774ba3f9f4a158a5
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-15 20:16:49 -0800

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array != NULL)
 	{
-		for (a = 0; a < size; a++)
+		for (a = 0; (a < size) && (array); a++)
 		{
 			printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 			if (array[a] == value)
